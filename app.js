c5b30f6516bec46ce9f87c8acc7c4f38983fa69d
micajank <micajank@umich.edu>
2020-07-23 14:55:25 -0700

    [Constructor function] Create NBA constructor function

--- app.js
+++ app.js
@@ -17,3 +17,27 @@ console.log(dog);
 console.log(dogTwo);
 
 console.log(dogTwo.goodBoi);
+
+
+// Constructor Function
+function User(name) {
+    // this is set equal to User
+    this.name = name;
+
+    // returns this
+}
+
+let mac = new User('Mac');
+let phil = new User('Phil');
+console.log(mac);
+console.log(phil);
+
+function NBoobAPlayer(name, boobean, threeNipShooter) {
+    this.name = name;
+    this.boobean = boobean;
+    this.threeNipShooter = threeNipShooter;
+}
+
+let cookies = new NBoobAPlayer('Mac', true, "Phil's mouth");
+console.log(cookies);
+console.log(cookies.threeNipShooter);
